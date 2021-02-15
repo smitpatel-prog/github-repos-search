a309e58ec4532b52c4f2afe36031c5299b9e8cc5
Smit Patel <smitp8464@gmail.com>
2021-02-15 12:53:06 +0530

    added env file

--- src/apis/github.js
+++ src/apis/github.js
@@ -2,5 +2,5 @@ import axios from "axios";
 
 export  default axios.create({
     baseURL: 'https://api.github.com/graphql',
-    headers: { Authorization: "bearer 592f2a247c9e3aa4f55eabf5834b94d5dafc97c2"} 
+    headers: { Authorization: `bearer   ${process.env.REACT_APP_AUTH_TOKEN} ` }
 });
